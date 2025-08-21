9a320436c715a9624ebd6196b4dc821871ef3895
Irene Ann Mathew <ireneann995@gmail.com>
2025-08-21 21:43:11 +1200

    Added Validation to ContactDto using Annotations

--- AddressBook.Application/DTO/ContactDto.cs
+++ AddressBook.Application/DTO/ContactDto.cs
@@ -1,18 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AddressBook.Application.DTO
 {
     public class ContactDto
     {
         public Guid? Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        [Required, StringLength(200)] public string FirstName { get; set; } = string.Empty;
+        [Required, StringLength(200)] public string LastName { get; set; } = string.Empty;
+        [Required, Phone] public string PhoneNumber { get; set; } = string.Empty;
+        [Required, EmailAddress] public string Email { get; set; } = string.Empty;
         public IEnumerable<Guid> GroupIds { get; set; } = new List<Guid>();
     }
 }
